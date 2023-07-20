d19d60d9aeeb84093da0bff1f5f3805ee9dd68c9
ItsFaizan <faizanjavid71@gmail.com>
2023-07-20 23:55:32 +0500

    user logged in

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {auth} from "../config/firebase";
+import { createUserWithEmailAndPassword} from 'firebase/auth'
 
 export const Auth = () => {
-  return (
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const signIn = async () =>{
+         await createUserWithEmailAndPassword(auth, email, password); 
+    }
+  return (  
     <div>
-        <input placeholder='Email'/>
-        <input placeholder='Password'/>
+        <input placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+        <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={signIn}> Sign In</button>
     </div>
   )
 }
