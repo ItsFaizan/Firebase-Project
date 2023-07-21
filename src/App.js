e8bd9c5742f5283082633a08d969355c516c1b25
ItsFaizan <faizanjavid71@gmail.com>
2023-07-21 16:56:02 +0500

    Food info displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import { Auth } from "./components/auth.js";
+import {db} from "./config/firebase";
+import {getDocs, collection} from "firebase/firestore"
 
 function App() {
+  const[foodlist, setFoodlist] = useState([]);
+  const foodCollectionRef = collection(db, "Food");
+
+  useEffect(() =>{
+    const getFoodlist = async () => {
+      const data = await getDocs(foodCollectionRef)
+      const filterData = data.docs.map((doc) =>({
+          ...doc.data(),
+          id: doc.id,
+      }));
+      setFoodlist(filterData);
+    };
+     getFoodlist();
+     
+  }, [])
   return (
     <div>
       <Auth/>
+      
+      <div>
+        
+        {foodlist.map((food) => (
+          <div>
+            <h1 style={{color:food.deliveryAvailable? "green ": "red" }}>{food.name}</h1>
+            <h1>{food.quantity}</h1>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCjsfsj4GOcmwxu0OHZTJfxzDCROIiMPLo",
   authDomain: "fir-project-65e1f.firebaseapp.com",
@@ -13,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
