6d5c9c343065a1c8884d9c14537678db06bcc0ca
ItsFaizan <faizanjavid71@gmail.com>
2023-07-21 14:42:41 +0500

    Google authentication added

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
-import {auth} from "../config/firebase";
-import { createUserWithEmailAndPassword} from 'firebase/auth'
+import {auth, googleProvider} from "../config/firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 
 export const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
     const signIn = async () =>{
          await createUserWithEmailAndPassword(auth, email, password); 
     }
+
+    const signInwithGoogle = async () =>{
+      await signInWithPopup(auth, googleProvider ); 
+ }
+
+ const logout = async () =>{
+  await signOut(auth); 
+}
   return (  
     <div>
         <input placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
         <input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
         <button onClick={signIn}> Sign In</button>
+        <button onClick={signInwithGoogle}> SignIn with Google </button>
+        <button onClick={logout}> SignOut </button>
     </div>
   )
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCjsfsj4GOcmwxu0OHZTJfxzDCROIiMPLo",
   authDomain: "fir-project-65e1f.firebaseapp.com",
@@ -12,3 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
