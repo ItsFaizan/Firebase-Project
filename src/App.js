d1db89bb6d0d809af6e99dfe8c5e20011d989462
ItsFaizan <faizanjavid71@gmail.com>
2023-07-22 19:18:26 +0500

    New food added to database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { Auth } from "./components/auth.js";
 import {db} from "./config/firebase";
-import {getDocs, collection} from "firebase/firestore"
+import {getDocs, collection , addDoc} from "firebase/firestore"
 
 function App() {
   const[foodlist, setFoodlist] = useState([]);
+  const [foodname, setFoodname] = useState("");
+  const [foodquantity, setFoodquantity] = useState(0);
+  const [delivery, setDelivery] = useState(false);
+
   const foodCollectionRef = collection(db, "Food");
 
   useEffect(() =>{
@@ -19,15 +23,28 @@ function App() {
      getFoodlist();
      
   }, [])
+
+  const onSubmitFood = async () => [
+    await addDoc(foodCollectionRef, {
+      name: foodname,
+      quantity: foodquantity,
+      delivery: delivery,
+    })
+  ]
   return (
     <div>
       <Auth/>
-      
       <div>
-        
+        <input placeholder="Food Name" onChange={(e) => setFoodname(e.target.value)}/>
+        <input placeholder="Food Quantity" type="number" onChange={(e) => setFoodquantity(Number(e.target.value))}/>
+        <input type="checkbox" onChange={(e) => setDelivery(e.target.checked)}/>
+        <label>delivery Available</label>
+        <button onClick={onSubmitFood}>Send Food</button>
+      </div>
+      <div>
         {foodlist.map((food) => (
           <div>
-            <h1 style={{color:food.deliveryAvailable? "green ": "red" }}>{food.name}</h1>
+            <h1 style={{color: food.deliveryAvailable? "green ": "red" }}>{food.name}</h1>
             <h1>{food.quantity}</h1>
             </div>
         ))}
